4fbe948208eb0d8946ef084128cdc9ec82c5d125
ronbp <ronmagpantay9513@gmail.com>
2021-04-23 00:44:46 -0700

    App.js + App.css: Skills section is completed and looking fantastic!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Card, Button, Navbar, Nav } from 'react-bootstrap'
+import html from './assets/logos/htmlLogo.png'
+import css from './assets/logos/cssLogo.jpeg'
+import javascript from './assets/logos/javascriptLogo.png'
+import reactlogo from './assets/logos/reactLogo.png'
+import postgreslogo from './assets/logos/postgresLogo.png'
+import nodelogo from './assets/logos/nodeLogo.png'
+import expresslogo from './assets/logos/expressLogo.png'
+import pythonlogo from './assets/logos/pythonLogo.png'
 
 function App() {
   return (
@@ -12,6 +20,7 @@ function App() {
           <Nav.Link className="navbutton"><strong>skills</strong></Nav.Link>
           <Nav.Link className="navbutton"><strong>projects</strong></Nav.Link>
           <Nav.Link className="navbutton"><strong>about</strong></Nav.Link>
+          <Nav.Link className="navbutton"><strong>contact</strong></Nav.Link>
         </Nav>
       </Navbar>
 
@@ -25,64 +34,87 @@ function App() {
       </div>
 
       <div className="skills">
-        <h1><strong>Skills / Tech Stack</strong></h1>
-        <h2>😎skill 1</h2>
-        <h2>😎skill 2</h2>
-        <h2>😎skill 3</h2>
-        <h2>😎skill 4</h2>
+        
+        <p id="skilltitle">Skills</p>
+
+        <div className="skillcontainers">
+          
+          <div className="skillcontainer1">
+          <img className="logos" src={html}/>
+          <h2 className="techskill">HTML</h2>
+          <img className="logos" src={css}/>
+          <h2 className="techskill">CSS</h2>
+          <img className="logos" src={javascript}/>
+          <h2 className="techskill">Javascript</h2>
+          <img className="logos" src={reactlogo}/>
+          <h2 className="techskill">React</h2>
+          </div>
+          
+          <div className="skillcontainer2">
+          <img className="logos" src={postgreslogo}/>
+          <h2 className="techskill">Postgres</h2>
+          <img className="logos" src={nodelogo}/>
+          <h2 className="techskill">Node</h2>
+          <img className="logos" src={expresslogo}/>
+          <h2 className="techskill">Express</h2>
+          <img className="logos" src={pythonlogo}/>
+          <h2 className="techskill">Python</h2>  
+          </div>
+        
+        </div>
       </div>
 
-      <div className="topcards">
-        <Card>
-          <Card.Img />
-          <Card.Body>
-            <Card.Title>
-              Pokemon
-            </Card.Title>
-            <Card.Text>
-              Battle Simulator built in Vanilla JS
-            </Card.Text>
-            <div className="cardcontainer">
-            <Button className="carditem">Frontend</Button>
-            <Button className="carditem">Backend</Button>
-            </div>
-          </Card.Body>
-        </Card>
+      <div className="projectscontainer">
+        <h1>Projects</h1>
 
-        <Card>
-          <Card.Img />
-          <Card.Body>
-            <Card.Title>
-              Country Companion
-            </Card.Title>
-            <Card.Text>
-              Country Data and Currency built with Express
-            </Card.Text>
-            <div className="cardcontainer">
-            <Button className="carditem">Frontend</Button>
-            <Button className="carditem">Backend</Button>
-            </div>
-          </Card.Body>
-        </Card>
+          <Card className="actualcard">
+            <Card.Img />
+            <Card.Body className="cardbody">
+              <Card.Title>
+                Pokemon
+              </Card.Title>
+              <Card.Text>
+                Battle Simulator built in Vanilla JS
+              </Card.Text>
+              <div className="cardcontainer">
+              <Button className="carditem">Frontend</Button>
+              <Button className="carditem">Backend</Button>
+              </div>
+            </Card.Body>
+          </Card>
 
+          <Card className="actualcard">
+            <Card.Img />
+            <Card.Body className="cardbody">
+              <Card.Title>
+                Country Companion
+              </Card.Title>
+              <Card.Text>
+                Country Data and Currency built with Express
+              </Card.Text>
+              <div className="cardcontainer">
+              <Button className="carditem">Frontend</Button>
+              <Button className="carditem">Backend</Button>
+              </div>
+            </Card.Body>
+          </Card>
+
+          <Card className="actualcard">
+            <Card.Img />
+            <Card.Body className="cardbody">
+              <Card.Title>
+                React 1UP
+              </Card.Title>
+              <Card.Text>
+                Productivity App built in React
+              </Card.Text>
+              <div className="cardcontainer">
+              <Button className="carditem">Frontend</Button>
+              <Button className="carditem">Backend</Button>
+              </div>
+            </Card.Body>
+          </Card>
       </div>
-
-        <Card id="bottomcard">
-          <Card.Img />
-          <Card.Body>
-            <Card.Title>
-              React 1UP
-            </Card.Title>
-            <Card.Text>
-              Productivity App built in React
-            </Card.Text>
-            <div className="cardcontainer">
-            <Button className="carditem">Frontend</Button>
-            <Button className="carditem">Backend</Button>
-            </div>
-          </Card.Body>
-        </Card>
-
 
       <div className="about">
         <h2>About Me Information goes down here</h2>
